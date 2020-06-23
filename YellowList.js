f14ee744e86dd16e3045bf0d2bf399a864c999f4
ouchi-dqx <sinigamigeme@gmail.com>
2020-06-23 20:16:13 +0900

    update

--- YellowList.js
+++ YellowList.js
@@ -1,41 +1,41 @@
-function Getter(){
+function Getter(num){
     $.ajax({
         url: "https://script.google.com/macros/s/AKfycbxlGCRghpYCAy7eyk0baCalwF0ZXjG_6tI-ZRVXdeiEo5kpUcw/exec",
         type: "GET",
         dataType: "jsonp",
         data: {mode: "read"},
         success: res => {
-            Loader(res.Array)
+            Loader(res.Array,num)
         }
     })
 }
 
-function Loader(Data){    
+function Loader(Data,num){
     $(".ServerList tr").slice(1).remove() //テーブルの初期化
 
-    //サーバー行・時間追加
-    var CopyTemp,ServerList
-    var tmp1 = document.getElementById("template1")
-    for(var i=0; i<40; i++){
-        CopyTemp = tmp1.content.cloneNode(true)
-        ServerList = document.getElementsByClassName("ServerList")
-        ServerList[0].appendChild(CopyTemp)
+    //サーバー行追加
+    var CopyTemp,Point,check2,cnt
+    var t = new Date()
+    var check1 = ("0" + (t.getMonth() + 1)).slice(-2) + ""
+                + ("0" + t.getDate()).slice(-2) + ""
+                + ("0" + t.getHours()).slice(-2) + ""
+                + ("0" + t.getMinutes()).slice(-2)
+    num = Number(num.value) //数値型変換
+    if(num != 0){ cnt = 10 }else{ num = 1;cnt = 40 }
+
+    for(var Server=num; Server<num + cnt; Server++){
+        CopyTemp = $($("#template1").html()).clone()
+        CopyTemp.find(".Server").text("サーバー" + (Server))
+        Point = 0 //初期化
+        CopyTemp.find("p>.setTemp").each(function(){
+            check2 = Data[Server - 1][Point].replace(/:|\s|\//g, "")
+            if(check1 < check2){ //現在時間より先なら表示
+                $(this).text(Data[Server - 1][Point])
+            }else{
+                $(this).text("--:--:--")
+            }
+            Point++
+        })
+        $(".ServerList").append(CopyTemp)
     }
-
-    var Server = 0
-    $(".Servers").each(function(){
-        $(this).find(".Server").text("サーバー" + Number(Server + 1))
-
-        //if(Data[Server][0]){
-            $(this).find(".ゲル>p>.setTemp").text(Data[Server][0])
-        //}
-        //if(Data[Server][1]){
-            $(this).find(".砂漠>p>.setTemp").text(Data[Server][1])
-        //}
-        //if(Data[Server][2]){
-            $(this).find(".バル>p>.setTemp").text(Data[Server][2])
-        //}
-        
-        Server++
-    })
 }
