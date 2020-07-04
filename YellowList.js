da311a4a8a3ae8c56a09b9a20c0e9c0e3bca6aa3
ouchi-dqx <sinigamigeme@gmail.com>
2020-07-04 16:03:32 +0900

    update

--- YellowList.js
+++ YellowList.js
@@ -14,12 +14,12 @@ function Loader(Data,num){
     $(".ServerList tr").slice(1).remove() //テーブルの初期化
 
     //サーバー行追加
-    var CopyTemp,Point,check2,cnt
-    var t = new Date()
-    var check1 = ("0" + (t.getMonth() + 1)).slice(-2) + ""
-                + ("0" + t.getDate()).slice(-2) + ""
-                + ("0" + t.getHours()).slice(-2) + ""
-                + ("0" + t.getMinutes()).slice(-2)
+    var CopyTemp,Point,check3,cnt
+    var check1 = new Date().getTime()
+    var check2 = new Date()
+    check2.setHours(check2.getHours() -1)
+    check2.setMinutes(check2.getMinutes() -30)
+    check2 = check2.getTime()
     num = Number(num.value) //数値型変換
     if(num != 0){ cnt = 10 }else{ num = 1;cnt = 40 }
 
@@ -27,12 +27,19 @@ function Loader(Data,num){
         CopyTemp = $($("#template1").html()).clone()
         CopyTemp.find(".Server").text("サーバー" + (Server))
         Point = 0 //初期化
-        CopyTemp.find("p>.setTemp").each(function(){
-            check2 = Data[Server - 1][Point].replace(/:|\s|\//g, "")
-            if(check1 < check2){ //現在時間より先なら表示
-                $(this).text(Data[Server - 1][Point])
+        CopyTemp.find("p").each(function(){
+            check3 = new Date("2020/" + Data[Server - 1][Point]).getTime()
+
+            if(check1 < check3){ //現在時間より先なら表示
+                $(this).text(Data[Server - 1][Point] + "まで")
+                $(this).parents("td").css("background-color", "yellow")
             }else{
-                $(this).text("--:--:--")
+                if(check2 < check3){ //現在時間-1時間半より先なら表示
+                    //$(this).text(Data[Server - 1][Point] + "")
+                    //$(this).parents("td").css("background-color", "yellow")
+                }else{
+                    $(this).text("--:--:--")
+                }
             }
             Point++
         })
