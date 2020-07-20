87f1019db57045c709bace17a98917fe5aa62ba7
ouchi-dqx <sinigamigeme@gmail.com>
2020-07-21 01:00:41 +0900

    fix

--- src/kishoju.js
+++ src/kishoju.js
@@ -728,6 +728,7 @@ function TimePlus(Time, sumTime){
 function setTimer(objBox){
     const Server = objBox.closest("tr").find(".Server").text()
     const Point = objBox.closest("td").attr("class")
+    const memo = objBox.find(".memo").text()
 
     const newDate = new Date().getTime()
     const nowDate = objBox.find(".nowTime").attr("Date")
@@ -747,7 +748,7 @@ function setTimer(objBox){
             .css("background-color", "violet")
             .attr("color", "violet")
     }else
-    if((1000 * 60 * 60) <= diffTime){
+    if((1000 * 60 * 60) <= diffTime && memo != "経過時間:01:00:00") {
         objBox.find(".befTime")
             .attr("Date", nowDate)
             .text(nowTime)
@@ -770,7 +771,7 @@ function setTimer(objBox){
         clearInterval(Timers[Server + Point])
         clear_one_fix("fix_red",Server + Point)
     }else
-    if(objBox.find(".memo").text() == "経過時間:01:00:00"){
+    if(memo == "経過時間:01:00:00"){
         clearInterval(Timers[Server + Point])
     }else{
         objBox.find(".memo").text("経過時間:" + Time)
