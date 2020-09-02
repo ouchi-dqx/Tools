6a2c5df83c189b4621beea8d0c74858f73522545
ouchi-dqx <sinigamigeme@gmail.com>
2020-09-03 00:36:18 +0900

    add

---
+++ src/kishoju_room.js
@@ -0,0 +1,34 @@
+alert("aa");
+
+/*
+
+$(function(){
+    const observer = new MutationObserver(callback)
+    function callback(mutations){
+        mutations.forEach(mutation => {
+            mutation.addedNodes.forEach(node => {
+                alert(node.innerText)
+            })
+            mutation.removedNodes.forEach(node => {
+                alert(node.innerText)
+            })
+        })
+    }
+
+
+    observer.observe($(document).$(".fix_blue")[0], {
+        childList: true,
+        attributes: true,
+        characterData: true,
+        subtree: true
+    });
+
+    observer.observe($(document).$(".fix_red")[0], {
+        childList: true,
+        attributes: true,
+        characterData: true,
+        subtree: true
+    })
+})
+
+*/
