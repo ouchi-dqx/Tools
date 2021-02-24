4a4e1a76425183d4a5d77adc1d919e2db62a0f0f
ouchi-dqx <sinigamigeme@gmail.com>
2021-02-24 22:36:58 +0900

    update とりあえずの実装

--- src/getAnalytics.js
+++ src/getAnalytics.js
@@ -1,11 +1,75 @@
 function debug() {
+    let Time = new Date();
+    Time.setDate(Time.getDate() - 1)
+    Time = Time.getFullYear() + "/" +
+        ("0" + (Time.getMonth() + 1)).slice(-2) + "/" +
+        ("0" + Time.getDate()).slice(-2)
+
     const params = {
         mode: "getAnalytics",
-        getDate: "2021/02/22",
+        getDate: Time
     }
 
     xhrSend(params, (res) => {
-        $(".message").text(res[0].Data)
+        const myData = document.getElementById('AnalyticsChart');
+
+        let Times = [];
+        for (let i = 0; i <= 23; i++) Times.push(i + "時");
+
+        const chart = new Chart(myData, {
+            type: "line",
+            data: {
+                labels: Times,
+                datasets: [
+                    {
+                        label: '昨日のユーザー数',
+                        backgroundColor: "rgb(10, 150, 190)",
+                        borderColor: "rgb(10, 150, 190)",
+                        data: res[0].Data.split(","),
+                        lineTension: 0,
+                        fill: false,
+                    },
+                    {
+                        label: '一昨日のユーザー数',
+                        backgroundColor: "rgb(10, 150, 190)",
+                        borderColor: "rgb(10, 150, 190)",
+                        borderDash: [5, 3],
+                        data: [0],
+                        lineTension: 0,
+                        fill: false,
+                        hidden: true,
+                    }
+                ]
+            },
+            options: {
+                scales: {
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "時"
+                        }
+                    }],
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: "人"
+                        }
+                    }]
+                },
+                tooltips: {
+                    filter: (item, Data) => {
+                        const label = Data.datasets[item.datasetIndex].label;
+                        if (label == "昨日のユーザー数") return true;
+                        if (label == "一昨日のユーザー数") return true;//false
+                    },
+                    callbacks: {
+                        afterLabel: (item, Data) => {
+                            //return "test"
+                        },
+                    }
+                }
+            }
+        })
     })
 }
 
